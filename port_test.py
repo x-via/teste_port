f54ec79e4f39f84e6dcd8eef54f607d02d9747c8
Asantosjorge <127167561+Asantosjorge@users.noreply.github.com>
2024-09-24 17:09:51 -0300

    Update port_test.py

--- port_test.py
+++ port_test.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 import socket
-import subprocess
 import dns.resolver  # Para resolver nomes DNS
 
 app = Flask(__name__)
+app.secret_key = 'sua_chave_secreta'  # Necessária para usar mensagens flash
+
+# Lista para armazenar os DNS cadastrados
+dns_list = []
 
 def is_port_open(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -18,30 +21,25 @@ def resolve_dns(name):
     except Exception as e:
         return None
 
-def check_route(host, method):
-    if method == "dig":
-        try:
-            result = subprocess.run(['dig', host], capture_output=True, text=True)
-            return result.stdout
-        except Exception as e:
-            return str(e)
-    elif method == "tracert":
-        try:
-            result = subprocess.run(['traceroute', host], capture_output=True, text=True)
-            return result.stdout
-        except Exception as e:
-            return str(e)
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', dns_list=dns_list)
+
+@app.route('/register_dns', methods=['POST'])
+def register_dns():
+    dns_name = request.form['dns_name']
+    if dns_name and dns_name not in dns_list:
+        dns_list.append(dns_name)
+        flash(f'DNS "{dns_name}" cadastrado com sucesso!')
+    else:
+        flash('Erro ao cadastrar DNS ou DNS já cadastrado!')
+    return redirect(url_for('index'))
 
 @app.route('/check', methods=['POST'])
 def check_ports():
     host = request.form['host']
     ports = request.form['ports'].split(',')
-    dns_name = request.form.get('dns_name')
-    route_method = request.form.get('route_check')
+    selected_dns = request.form.get('selected_dns')
 
     results = []
     for port in ports:
@@ -50,20 +48,17 @@ def check_ports():
         results.append({'port': port, 'is_open': is_open})
 
     dns_resolution = None
-    if dns_name:
-        dns_resolution = resolve_dns(dns_name)
-
-    route_result = check_route(host, route_method)
+    if selected_dns:
+        dns_resolution = resolve_dns(selected_dns)
 
     return render_template(
         'result.html',
         host=host,
         results=results,
-        dns_name=dns_name,
-        dns_resolution=dns_resolution,
-        route_method=route_method,
-        route_result=route_result
+        selected_dns=selected_dns,
+        dns_resolution=dns_resolution
     )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1000)
+
